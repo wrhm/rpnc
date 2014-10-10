ad0605ef0f3bfcad5a1dc5a16ec53fc774bd8384
wrhm <bmatchen@gmail.com>
2014-10-10 18:56:03 -0400

    implemented trace, enabled help

--- polish_calc.py
+++ polish_calc.py
@@ -35,11 +35,11 @@ def rpcVal(expr,trace):
         if isNum(c): #number (natural or decimal)
             values.append(float(c))
         elif c=='phi': #constant
-            values = values+[rpcVal('5 1 2 / ^ 1 + 2 /',False)]
+            values = values+[rpcVal('5 1 2 / ^ 1 + 2 /',trace)]
         elif len(values)==0: return 'Need at least one value.'
         elif len(values)<=1: #unary operators
             if c=='fib': # overflows if argument > 1474
-                values = values[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(values[-1],values[-1]),False)]
+                values = values[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(values[-1],values[-1]),trace)]
             else:
                 if c in binary:
                     return 'Not enough values.'
@@ -63,6 +63,7 @@ def rpcVal(expr,trace):
             #        return '%s needs an argument.'%c
         tokens = tokens[1:]
     #print 'tokens empty.'
+    if trace: print 'tokens: %s, values: %s'%(tokens,values)
     if len(values)==1:
         if abs(values[0]-float(int(values[0])))<=1e-9: #truncate if (almost) whole number
             return int(values[0])
@@ -73,22 +74,28 @@ def rpcVal(expr,trace):
     #    print len(values)
         return 'Not enough operators.'
 
+#TESTS
+assert True
+
+
 if __name__ == "__main__":
-    s = ''
     print '%s\n| RPN Calculator |\n%s'%('='*18,'='*18)
-    print 'Valid operators: + - * / ^'
-    print 'Valid values: naturals and decimals, and'
-    print '   Constants: phi'
+    help = ('Valid operators: + - * / ^\n' + 
+     'Valid values: naturals and decimals, and\n' +
+     '   Constants: phi\n' +
+     'Commands: \'trace\', \'exit\', \'help\'')
+    print help
     #help = 'Enter an expression, \'help\', or \'Exit\''
     #print help
     trace = False
     while True:
-        #s = '3 5 + 7 / 4 * 2 -'
         s = raw_input('>> ')
         if 'exit' in string.lower(s):
             sys.exit()
-        if 'trace' in string.lower(s):
+        if 'help' in string.lower(s):
+            print help
+        elif 'trace' in string.lower(s):
             trace = not trace
-            print 'Trace mode now %s.'%('on' if trace else 'off')
+            print '   [Trace mode now %s.]'%('on' if trace else 'off')
         else:
             print '   = %s'%rpcVal(s,trace)
