c075d157c46642d282e6a78e6d7630d7a5cd379a
wrhm <bmatchen@gmail.com>
2014-10-10 18:38:07 -0400

    improved error feedback

--- polish_calc.py
+++ polish_calc.py
@@ -22,53 +22,55 @@ def isNum(s):
             return False
     return True
 
-def rpcVal(expr):
+def rpcVal(expr,trace):
     unary = ['fib']
-    vals = expr.split()
-    if len(vals)==0: #print 'Invalid input.'
-        return 'Need at least one value.'
-    t = []
-    while len(vals)>0:
-        #print 'vals: %s, t: %s'%(vals,t)
-        c = vals[0]
+    binary = ['+','-','*','/','^']
+    tokens = expr.split()
+    if len(tokens)==0:
+        return 'Need at least one token.'
+    values = []
+    while len(tokens)>0:
+        if trace: print 'tokens: %s, values: %s'%(tokens,values)
+        c = tokens[0]
         if isNum(c): #number (natural or decimal)
-            t = t + [float(c)]
+            values.append(float(c))
         elif c=='phi': #constant
-            t = t+[rpcVal('5 1 2 / ^ 1 + 2 /')]
-        elif len(t)==1: #unary operators
+            values = values+[rpcVal('5 1 2 / ^ 1 + 2 /',False)]
+        elif len(values)==0: return 'Need at least one value.'
+        elif len(values)<=1: #unary operators
             if c=='fib': # overflows if argument > 1474
-                t = t[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(t[-1],t[-1]))]
-            #if c== '!':
-            #    l = 
-            #    t = t[:-2]
-        else: #binary operators
-            if c=='+': #print 'Time to add.'
-                t = t[:-2]+[t[-2]+t[-1]]
-            elif c=='-': #print 'Time to subtract.'
-                t = t[:-2]+[t[-2]-t[-1]]
-            elif c=='*': #print 'Time to multiply.'
-                t = t[:-2]+[t[-2]*t[-1]]
-            elif c=='/': #print 'Time to divide.'
-                if t[-1]==0:
-                    return 'Error: division by zero.' #return None
-                t = t[:-2]+[t[-2]/t[-1]]
-            elif c=='^':
-                t = t[:-2]+[t[-2]**t[-1]]
+                values = values[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(values[-1],values[-1]),False)]
             else:
-                if c in unary:
-                    return '%s needs an argument.'%c
+                if c in binary:
+                    return 'Not enough values.'
                 else:
-                    return 'Unrecognized symbol: %s.'%c #return None
-        vals = vals[1:]
-    #print 'vals empty.'
-    if len(t)==1:
-        #return t[0]
-        if abs(t[0]-float(int(t[0])))<=1e-9: #truncate if (almost) whole number
-            return int(t[0])
+                    return 'Unrecognized symbol: %s.'%c
+        else:#elif len(values)==2: #binary operators
+            if c=='+': #print 'Time to add.'
+                values = values[:-2]+[values[-2]+values[-1]]
+            elif c=='-': #print 'Time to subtract.'
+                values = values[:-2]+[values[-2]-values[-1]]
+            elif c=='*': #print 'Time to multiply.'
+                values = values[:-2]+[values[-2]*values[-1]]
+            elif c=='/': #print 'Time to divide.'
+                if values[-1]==0:
+                    return 'Error: division by zero.' #return None
+                values = values[:-2]+[values[-2]/values[-1]]
+            elif c=='^':
+                values = values[:-2]+[values[-2]**values[-1]]
+            #else:
+            #    if c in unary:
+            #        return '%s needs an argument.'%c
+        tokens = tokens[1:]
+    #print 'tokens empty.'
+    if len(values)==1:
+        if abs(values[0]-float(int(values[0])))<=1e-9: #truncate if (almost) whole number
+            return int(values[0])
         else:
-            return t[0]
+            return values[0]
     else:
         #return None
+    #    print len(values)
         return 'Not enough operators.'
 
 if __name__ == "__main__":
@@ -79,10 +81,14 @@ if __name__ == "__main__":
     print '   Constants: phi'
     #help = 'Enter an expression, \'help\', or \'Exit\''
     #print help
+    trace = False
     while True:
         #s = '3 5 + 7 / 4 * 2 -'
         s = raw_input('>> ')
         if 'exit' in string.lower(s):
             sys.exit()
+        if 'trace' in string.lower(s):
+            trace = not trace
+            print 'Trace mode now %s.'%('on' if trace else 'off')
         else:
-            print '   = %s'%rpcVal(s)
+            print '   = %s'%rpcVal(s,trace)
