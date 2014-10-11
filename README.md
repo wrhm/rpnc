f4ebb7865612c2180be29d787c628332e5f0defe
wrhm <bmatchen@gmail.com>
2014-10-11 19:42:09 -0400

    nested op counting; included quad1, quad2, hyp, amean, gmean

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 Reverse Polish Notation Calculator (rpnc)
 
 Uses Python to implement an RPN (postfix) calculator.
-Currently allows addition, subtraction, multiplication, division, and exponentiation.
-Also has built-in constants (e, pi, phi) and custom unary operators (fib). Constants are either exact or decent aymptotic approximations.
-
-Still want: ternary quadratic root operator.
+Currently allows these operations:
+ - Unary - fib
+ - Binary - addition, subtraction, multiplication, division, and exponentiation; arithmetic and geometric means.
+ - Ternary - quad1, quad 2
+Also has built-in constants (e, pi, phi). Constants are either exact or decent aymptotic approximations.
--- polish_calc.py
+++ polish_calc.py
@@ -14,25 +14,47 @@ import string, sys
 17
 '''
 
+'''
+TO BE IMPLEMENTED:
+
+a b c quad1: rpcVal('0 b - b ^ 2 4 a c * * - 1 2 / ^ - 2 a * /',trace)
+a b c quad2: rpcVal('0 b - b ^ 2 4 a c * * - 1 2 / ^ + 2 a * /',trace)
+
+x y w z dist: rpcVal('x w - 2 ^ y z - 2 ^ + 1 2 / ^',trace)
+x y hyp:  rpcVal('x 2 ^ y 2 ^ 1 2 / ^',trace)
+        = rpcVal('x y 0 0 dist',trace) 
+
+a b amean = rpcVal('a b + 2 /',trace)
+a b gmean = rpcVal('a b * 1 2 / ^',trace)
+'''
+
 def isNum(s):
     s = str(s)
+    if s.count('.')>1: return False
     d = string.digits+'.'
     for c in s:
         if c not in d:
             return False
     return True
 
+def isWhole(num):
+    return (abs(num-float(int(num)))<=1e-9)
+
 def rpcVal(expr,trace):
-    constants = ['pi','phi']
+    constants = ['e','pi','phi']
     unary = ['fib']
     binary = ['+','-','*','/','^']
-    ternary = ['quad1']
+    ternary = ['quad1', 'quad2', 'hyp','amean','gmean']
     tokens = expr.split()
     if len(tokens)==0:
         return 'Need at least one token.'
+    if 'ops?' in expr:
+        return [', '.join(constants),(', '.join(binary))+', '+(', '.join(unary))+', '+(', '.join(ternary))]
     values = []
     while len(tokens)>0:
-        if trace: print 'tokens: %s, values: %s'%(tokens,values)
+        if trace:
+            print ' - values: %s,\n   tokens: %s'%(values,tokens)
+            #print ' - values: %s'%values
         c = tokens[0]
         if isNum(c): #number (natural or decimal)
             values.append(float(c))
@@ -42,40 +64,73 @@ def rpcVal(expr,trace):
             values.append(rpcVal('245850922 78256779 /',trace))
         elif c=='phi': #constant
             values.append(rpcVal('5 1 2 / ^ 1 + 2 /',trace))
-        elif len(values)==0: return 'Need at least one value.'
-        elif len(values)<=1: #unary operators
-            if c=='fib': # overflows if argument > 1474
-                values = values[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(values[-1],values[-1]),trace)]
-            else:
-                if c in binary+ternary:
-                    return 'Not enough values for %s.'%c
-                else:
-                    return 'Unrecognized symbol: \'%s\'.'%c
-        else:#elif len(values)==2: #else: #binary operators
-            if c=='+': #print 'Time to add.'
-                values = values[:-2]+[values[-2]+values[-1]]
-            elif c=='-': #print 'Time to subtract.'
-                values = values[:-2]+[values[-2]-values[-1]]
-            elif c=='*': #print 'Time to multiply.'
-                values = values[:-2]+[values[-2]*values[-1]]
-            elif c=='/': #print 'Time to divide.'
-                if values[-1]==0:
-                    return 'Error: division by zero.' #return None
-                values = values[:-2]+[values[-2]/values[-1]]
-            elif c=='^':
-                values = values[:-2]+[values[-2]**values[-1]]
-            #else:
-            #    if c in unary:
-            #        return '%s needs an argument.'%c
-        #else: #ternary operators
-        #    if c=='quad1':
-        #        a,b,c = values[-3],values[-2],values[-1]
-        #        values = values[:-3] + [rpcVal('0 %s - %s 2 ^ 4 %s %s * * - 0.5 ^ - 2 %s * /'%(b,b,a,c,a),trace)]
+        elif len(values)==0: return 'At least one value is needed for %s.'%c
+        #elif len(values)>=1: #unary operators
+        else:
+            if len(values)>=1: #unary operators
+                if c=='fib': # overflows if argument > 1474
+                    values = values[:-2]+[rpcVal('phi %s ^ 0 phi - 0 %s - ^ - 5 1 2 / ^ /'%(values[-1],values[-1]),trace)]
+                '''else:
+                    if c in binary+ternary:
+                        return 'Not enough values for %s.'%c
+                    else:
+                        return 'Unrecognized symbol: \'%s\'.'%c '''
+                if len(values)>=2: #binary operators
+                    if c=='+':
+                        #print 'Time to add.'
+                        values = values[:-2]+[values[-2]+values[-1]]
+                    elif c=='-': #print 'Time to subtract.'
+                        values = values[:-2]+[values[-2]-values[-1]]
+                    elif c=='*': #print 'Time to multiply.'
+                        values = values[:-2]+[values[-2]*values[-1]]
+                    elif c=='/': #print 'Time to divide.'
+                        if values[-1]==0:
+                            return 'Error: division by zero.' #return None
+                        values = values[:-2]+[values[-2]/values[-1]]
+                    elif c=='^':
+                        b,p = values[-2],values[-1]
+                        if b<0 and not isWhole(p):
+                            return 'Error: negative raised to fractional power.'
+                        else:
+                            values = values[:-2]+[b**p]
+                    elif c=='hyp':
+                        pattern = '%s 2 ^ %s 2 ^ + 1 2 / ^'%(values[-2],values[-1])
+                        result = rpcVal(pattern,trace)
+                        if type(result) == str: return result
+                        else: values = values[:-2]+[result]
+                    elif c=='amean':
+                        pattern = '%s %s + 2 /'%(values[-2],values[-1])
+                        result = rpcVal(pattern,trace)
+                        if type(result) == str: return result
+                        else: values = values[:-2]+[result]
+                    elif c=='gmean':
+                        pattern = '%s %s * 1 2 / ^'%(values[-2],values[-1])
+                        result = rpcVal(pattern,trace)
+                        if type(result) == str: return result
+                        else: values = values[:-2]+[result]
+                    #else:
+                    #    if c in unary:
+                    #        return '%s needs an argument.'%c
+                    if len(values)>=3: #ternary operators
+                        if c=='quad1':
+                            a,b,c = values[-3],values[-2],values[-1]
+                            pattern = '0 %s - %s 2 ^ 4 %s %s * * - 0.5 ^ - 2 %s * /'%(b,b,a,c,a)
+                            result = rpcVal(pattern,trace)
+                            if type(result) == str: return result
+                            else: values = values[:-3]+[result]
+                        if c=='quad2':
+                            a,b,c = values[-3],values[-2],values[-1]
+                            pattern = '0 %s - %s 2 ^ 4 %s %s * * - 0.5 ^ + 2 %s * /'%(b,b,a,c,a)
+                            result = rpcVal(pattern,trace)
+                            if type(result) == str: return result
+                            else: values = values[:-3]+[result]
         tokens = tokens[1:]
     #print 'tokens empty.'
-    if trace: print 'tokens: %s, values: %s'%(tokens,values)
+    
+    #if trace: print 'tokens: %s, values: %s'%(tokens,values)
     if len(values)==1:
-        if abs(values[0]-float(int(values[0])))<=1e-9: #truncate if (almost) whole number
+        #if abs(values[0]-float(int(values[0])))<=1e-9: #truncate if (almost) whole number
+        if isWhole(values[0]):
             return int(values[0])
         else:
             return values[0]
@@ -87,8 +142,8 @@ def rpcVal(expr,trace):
 #TESTS
 #assert (rpcVal('',False) == )
 assert (rpcVal('',False) == 'Need at least one token.')
-assert (rpcVal('3 5 ^ 1 -',False) == 242)
-assert (rpcVal('6 1 / 0 *',False) == 0)
+#assert (rpcVal('3 5 ^ 1 -',False) == 242)
+#assert (rpcVal('6 1 / 0 *',False) == 0)
 '''
 assert (rpcVal('',False) == )
 assert (rpcVal('',False) == )
@@ -101,9 +156,10 @@ assert (rpcVal('',False) == )'''
 
 if __name__ == "__main__":
     print '%s\n| RPN Calculator |\n%s'%('='*18,'='*18)
-    help = ('Valid operators: + - * / ^\n' + 
+    ops = rpcVal('ops?',False)
+    help = ('Valid operators: %s\n'%ops[1] + 
      'Valid values: naturals and decimals, and\n' +
-     '   Constants: e, pi, phi\n' +
+     (' '*14)+'Constants: %s\n'%ops[0] +
      'Commands: \'trace\', \'exit\', \'help\'')
     print help
     #help = 'Enter an expression, \'help\', or \'Exit\''
